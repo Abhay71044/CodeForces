7d9901ff4eeb5cd881138f667aa74038499bcd47
Abhay Singh <227230915+Abhay71044@users.noreply.github.com>
2026-08-14 19:43:36 +0530

    Add In Search of an Easy Problem [A] from Codeforces

---
+++ 1030/A - In Search of an Easy Problem/solution.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    vector<int>arr(n);
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int flag=0;
+    for(int i=0;i<n;i++){
+        if(arr[i] == 1){
+            flag=1;
+            break;
+        }
+    }
+    if(flag==1){
+        cout<<"HARD";
+    }
+    else{
+        cout<<"EASY";
+    }
+    return 0;
+}
