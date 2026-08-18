e942171b730c24211d83d69955a6c07623c9faf9
Abhay Singh <227230915+Abhay71044@users.noreply.github.com>
2026-08-19 00:44:46 +0530

    Add HQ9+ [A] from Codeforces

---
+++ 133/A - HQ9+/solution.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string s;
+    cin>>s;
+    string ans="NO";
+    for(int i=0;i<s.size();i++){
+        if(s[i]=='H' || s[i]=='Q' || s[i]=='9'){
+            ans="YES";
+            break;
+        }
+        if(s[i] == '+' && ans=="YES"){
+            ans="YES";
+            break;
+        }
+    }
+    cout<<ans;
+    return 0;
+}
