8fa8e52e2fd9551a281c8450eb433064fa25f526
Abhay Singh <227230915+Abhay71044@users.noreply.github.com>
2026-09-04 15:24:52 +0530

    Add Bit++ [A] from Codeforces

---
+++ 282/A - Bit++/solution.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int ans=0;
+    while(n--){
+        string s;
+        cin>>s;
+        if(s=="X++" || s=="++X"){
+            ans+=1;
+        }
+        else{
+            ans-=1;
+        }
+    }
+    cout<<ans;
+    return 0;
+}
